0a7f8b8c36f410f2fe428eba374ced99f88cf2ee
samirr-0 <336754@student.mboutrecht.nl>
2024-09-30 13:48:29 +0200

    afl 3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>About Page</title>
 </head>
 <body>
 <nav>
     <a href="/">Home</a>
     <a href="/about">about</a>
     <a href="/contact">contact</a>
+    <a href="/meet-the-team">team</a>
 </nav>
 
-    <h1>Hello from the Home page.</h1>
+    <h1>Hello from the page.</h1>
+
+    {{ $slot }}
+    
 </body>
 </html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from the page </h1>
+</x-layout>

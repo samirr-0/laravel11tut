b0984551163d189306b693ea8f986e40daa17665
samirr-0 <336754@student.mboutrecht.nl>
2024-09-30 13:35:48 +0200

    afl 2

---
+++ resources/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+</nav>
+
+    <h1>Hello from the Home page.</h1>
+</body>
+</html>
